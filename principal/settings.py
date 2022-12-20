7edbf3e4b72a7eebd2b38d5d08a618c1af331ffb
nickisainez <nickiaasainez@gmail.com>
2022-12-20 12:34:44 -0500

    sqll

--- principal/settings.py
+++ principal/settings.py
@@ -86,6 +86,7 @@ WSGI_APPLICATION = 'principal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.mysql',
